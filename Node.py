d764b217bc562e00ce73728dbceb1b7cc453cbcd
chasel2361 <32896328+chasel2361@users.noreply.github.com>
2020-04-06 11:21:18 +0800

    Node and testNode

---
+++ Node.py
@@ -0,0 +1,168 @@
+﻿# -*- coding: utf-8 -*-
+"""author: 周光武、簡子琦"""
+from Vector import Vector
+from Point import Point
+from Dof import ActiveDof, BoundaryDof
+
+class Node:
+    """
+    Node object to be assigned to elements
+
+    Parameters
+    ----------
+    id : {element id, int}
+    p : {node coordinate, Point obj}
+
+    Returns
+    -------
+    out : Node obj
+
+    """
+    def __init__(self, id , p):
+        self._id = id
+        self._pos = p
+        self._dofs = [ActiveDof() for n in range(6)] 
+    
+    @property
+    def id(self):
+        return self._id
+    
+    @property
+    def dofs(self):
+        return self._dofs
+    
+    @property
+    def pos(self):
+        return self._get_pos('d')
+        
+    @property
+    def pos_last(self):
+        return self._get_pos('d_last')
+    
+    @property
+    def pos_try(self):
+        return self._get_pos('d_try')
+    
+    @property
+    def pos_try_last(self):
+        return self._get_pos('d_try_last')
+    
+    @property
+    def pos_origin(self):
+        return self._pos
+    
+###
+# 找時間改成restrain(type)
+    def restrain_dof(self, i):
+        """
+        Restrain specific dof in Node.
+
+        Parameters
+        ----------
+        i : {number of dof, int}
+
+        """
+        self.dofs[i] = BoundaryDof()
+        
+    def restrain(self):
+        """
+        Restrain all dofs in Node.
+
+        """
+        dofs = self.dofs
+        for i in range(6):
+            dofs[i] = BoundaryDof()
+###
+    
+    def assign_eq_number(self, eq_number):
+        """
+        Assign equation number to every ActiveDof in Node.
+
+        Parameters
+        ----------
+        eq_number : {equation number to be assign, int}
+
+        """
+        for dof in self._dofs:
+            if dof.is_restrained == True:
+                continue
+            else:
+                dof.eq_number = eq_number
+                eq_number += 1
+
+        return eq_number
+    
+    def dof(self, index):
+        """
+        Get value of specific dof in Node.
+
+        Parameters
+        ----------
+        index : {index of dof, int}
+
+        Returns
+        -------
+        out : float
+        """
+        return self._dofs[index]
+
+    def modify_dof(self, index, dof):
+        """
+        Modify specific dof.
+
+        Parameters
+        ----------
+        index : {index of dof, int}
+        dof : {new dof, dof obj}
+
+        """
+        self._dofs[index] = dof
+        
+    def integrate(self, integrator):
+        """
+        Integrate the dof value in Node.
+
+        Parameters
+        ----------
+        integrator : {time integrator, integrator obj}
+
+        """
+        for dof in self.dofs:
+            dof.integrate(integrator)
+            
+    def inc_d_try(self, delta_d):
+        """
+        Increase delta_d into d_try.
+
+        Parameters
+        ----------
+        delta_d : {increment of d_try, float}
+
+        """
+        for dof in self.dofs:
+            dof.inc_d_try(delta_d)
+    
+    def commit(self):
+        """
+        Commit all dofs value.
+        
+        """
+        for dof in self.dofs:
+            dof.commit()
+        
+        
+    def _get_pos(self, d_type):
+        dofs = self.dofs
+        pos = self._pos
+        return pos + Point(getattr(dofs[0], d_type), getattr(dofs[1], d_type), getattr(dofs[2], d_type))
+
+    def _get_dof(self, dof, d_type):
+        for d in self.dofs:
+            d._get_dof(dof, d_type)
+
+    def __repr__(self):
+        dof = self.dof
+        return (f'\n{self.__class__.__name__} '
+                f'id = {self.id}; '
+                f'pos = {self.pos.value}\n'
+                f'dofs = {dof(0), dof(1), dof(2)}\n       {dof(3), dof(4), dof(5)}')
---
+++ tests/testNode.py
@@ -0,0 +1,110 @@
+import unittest
+import random
+from Node import Node, BoundaryDof, ActiveDof, Point
+from Integrator import Newmark
+from math import isclose
+
+class NodeTest(unittest.TestCase):
+    def setUp(self):
+        self.node = Node(1, Point(1, 2, 3))
+    
+    def tearDown(self):
+        self.node = None
+    
+    #寫一個驗證is_restrained的test
+
+    #先驗證非restrained，鎖完之後再驗證是restrained
+    def test_restrain_dof(self):
+        n = self.node
+        restrain_dof = n.restrain_dof
+        expect = [type(BoundaryDof())]*6
+        Equal = self.assertEqual
+        for i in range(6):
+            restrain_dof(i)
+            Equal(type(n.dofs[i]), expect[i])
+    
+
+    def test_restrain(self):
+        n = self.node
+        expect = [type(BoundaryDof())]*6
+        Equal = self.assertEqual
+        n.restrain()
+        for i in range(6):
+            Equal(type(n.dofs[i]), expect[i])
+
+    def test_assign_eq_number(self):
+        n = self.node
+        Equal = self.assertEqual
+        n.restrain_dof(2)
+        n.restrain_dof(5)
+        n.assign_eq_number(1)
+        Equal(n.dofs[0].eq_number,  1)
+        Equal(n.dofs[1].eq_number,  2)
+        Equal(n.dofs[2].eq_number,  None)
+        Equal(n.dofs[3].eq_number,  3)
+        Equal(n.dofs[4].eq_number,  4)
+        Equal(n.dofs[5].eq_number,  None)
+    
+
+    def test_Newmark(self):
+        newmark = Newmark(0.01)
+        n = self.node
+        true = self.assertTrue        
+
+        for i in range(6):
+            dof = n.dofs[i]
+            dof.d_try = i*0.5 - 1.5
+            dof.v = i*0.5 - 1.5
+            dof.a = i*0.5 - 1.5
+
+        n.integrate(newmark)
+
+        dofs = n.dofs
+        true(isclose(dofs[0].a_try, -59398.5) and isclose(dofs[0].v_try, -298.5))
+        true(isclose(dofs[1].a_try, -39599.0) and isclose(dofs[1].v_try, -199.0))
+        true(isclose(dofs[2].a_try, -19799.5) and isclose(dofs[2].v_try, -99.5))
+        true(isclose(dofs[3].a_try, 0.0) and isclose(dofs[3].v_try, 0.0))
+        true(isclose(dofs[4].a_try, 19799.5) and isclose(dofs[4].v_try, 99.5))
+        true(isclose(dofs[5].a_try, 39599.0) and isclose(dofs[5].v_try, 199.0))
+        
+
+    def test_inc_d_try(self):
+        d = [2, 3, 4, 5, 6, 7]
+        n = self.node
+        Equal = self.assertEqual
+        dofs = n.dofs
+        n.assign_eq_number(0)
+        for i in range(6):
+            n.dofs[i].d_try = 1.0
+        
+        n.inc_d_try(d)
+
+        for i in range(6):
+            Equal(dofs[i].d_try_last, 1.0)
+            Equal(dofs[i].d_try, i+3)
+
+    def test_commit(self):
+        n = self.node
+        Equal = self.assertEqual
+        dis = [random.uniform(0, 3) for i in range(6)]
+        d_try = [random.uniform(0, 3) for i in range(6)]
+        v_try = [random.uniform(0, 3) for i in range(6)]
+        a_try = [random.uniform(0, 3) for i in range(6)]
+
+        for dof, d, dt, vt, at in zip(n.dofs, dis, d_try, v_try, a_try):
+            dof.d = d
+            dof.d_try = dt
+            dof.v_try = vt
+            dof.a_try = at
+        
+        n.commit()
+
+        for dof, d, dt, vt, at in zip(n.dofs, dis, d_try, v_try, a_try):
+            Equal(dof.d_last, d)
+            Equal(dof.d, dt)
+            Equal(dof.v, vt)
+            Equal(dof.a, at)
+
+
+if __name__ == '__main__':
+    unittest.main()
